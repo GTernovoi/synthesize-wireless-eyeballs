9a5c6350504fcb5142b1043d46df64ab538dd619
GTvoi <157132029+GTernovoi@users.noreply.github.com>
2024-04-21 03:16:46 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+        for (int i = 0; i < 10; i++) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+}
+
+        int[] data = new int[10];
+        for (int item : data) {
+    public static void main(String[] args) {
+        }
+
+            System.out.println("Random Number: " + item);
+        }main();
+
+    });
+function main() {
+    return data;
+    data.forEach(item => {
+    }
+
+}
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+
+    const data = [];
+}
+    const data = generateRandomData();
