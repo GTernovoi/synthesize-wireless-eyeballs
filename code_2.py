d48dec4a5cf71b796febdb9f63191c9ab89f8dad
GTvoi <157132029+GTernovoi@users.noreply.github.com>
2024-04-28 04:40:39 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,13 @@
-    }
-}
-        }
-        int[] data = new int[10];
 
-
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
     public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+}
         for (int item : data) {
         }
-            System.out.println("Random Number: " + item);
+
+        int[] data = new int[10];
+public class RandomDataGenerator {
         for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
